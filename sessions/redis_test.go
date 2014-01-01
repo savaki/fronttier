b9aff16066b7c1a2b19d2b7a24315e3bcd845aa6
Matt <matt.ho@gmail.com>
2013-12-31 20:06:36 -0800

    - redis is no only tested when REDIS_HOST is set

--- sessions/redis_test.go
+++ sessions/redis_test.go
@@ -2,84 +2,95 @@ package sessions
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"os"
 	"testing"
 )
 
 func TestRedis(t *testing.T) {
-	Convey("#Set", t, func() {
-		Convey("should set variables in redis", func() {
-			store := Redis().Namespace("testing").Build()
-			session := &Session{
-				Values: map[string]string{"hello": "world"},
-			}
-			store.Set("hello", session)
-
-			actual, err := store.Get("hello")
-
-			So(err, ShouldBeNil)
-			So(actual, ShouldNotBeNil)
-			So(actual.Values["hello"], ShouldEqual, "world")
-		})
-	})
-
-	Convey("#Create", t, func() {
-		Convey("should create a new session with a new sessionId and persist it into redis", func() {
-			store := Redis().Namespace("testing").Build()
-			values := map[string]string{"foo": "bar"}
-
-			// When
-			session, err := store.Create(values)
-
-			// Then
-			So(err, ShouldBeNil)
-			So(session, ShouldNotBeNil)
-
-			actual, _ := store.Get(session.Id)
-			So(actual, ShouldResemble, session)
-			So(actual.Values["foo"], ShouldEqual, "bar")
-		})
-	})
-
-	Convey("#Build", t, func() {
-		Convey("should default the namespace to 'sessions'", func() {
-			store := Redis().Build()
-			r := store.(*redis)
-
-			So(r.namespace, ShouldEqual, "sessions")
+	if os.Getenv("REDIS_HOST") == "" {
+		Convey("Redis tests are disabled", t, func() {
+			Convey("To test with redis set REDIS_HOST to the redis addr as follows:", func() {
+				Convey("export REDIS_HOST=localhost:6379", func() {
+				})
+			})
 		})
 
-		Convey("should assign #MaxIdle", func() {
-			maxIdle := 7
-			builder := Redis().MaxIdle(maxIdle)
+	} else {
+		Convey("#Set", t, func() {
+			Convey("should set variables in redis", func() {
+				store := Redis().Namespace("testing").Build()
+				session := &Session{
+					Values: map[string]string{"hello": "world"},
+				}
+				store.Set("hello", session)
 
-			So(builder.maxIdle, ShouldEqual, maxIdle)
+				actual, err := store.Get("hello")
+
+				So(err, ShouldBeNil)
+				So(actual, ShouldNotBeNil)
+				So(actual.Values["hello"], ShouldEqual, "world")
+			})
 		})
 
-		Convey("should assign #Host", func() {
-			host := "localhost:1234"
-			builder := Redis().Host(host)
+		Convey("#Create", t, func() {
+			Convey("should create a new session with a new sessionId and persist it into redis", func() {
+				store := Redis().Namespace("testing").Build()
+				values := map[string]string{"foo": "bar"}
 
-			So(builder.host, ShouldEqual, host)
+				// When
+				session, err := store.Create(values)
+
+				// Then
+				So(err, ShouldBeNil)
+				So(session, ShouldNotBeNil)
+
+				actual, _ := store.Get(session.Id)
+				So(actual, ShouldResemble, session)
+				So(actual.Values["foo"], ShouldEqual, "bar")
+			})
 		})
-	})
 
-	Convey("#Delete", t, func() {
-		Convey("should delete values from redis", func() {
-			store := Redis().Namespace("testing").Build()
-			values := map[string]string{"foo": "bar"}
+		Convey("#Build", t, func() {
+			Convey("should default the namespace to 'sessions'", func() {
+				store := Redis().Build()
+				r := store.(*redis)
 
-			// When
-			session, _ := store.Create(values)
-			err := store.Delete(session.Id)
+				So(r.namespace, ShouldEqual, "sessions")
+			})
 
-			// Then
-			So(err, ShouldBeNil)
-			So(session, ShouldNotBeNil)
-			So(session.Id, ShouldNotEqual, "")
+			Convey("should assign #MaxIdle", func() {
+				maxIdle := 7
+				builder := Redis().MaxIdle(maxIdle)
 
-			value, err := store.Get(session.Id)
-			So(err, ShouldBeNil)
-			So(value, ShouldBeNil)
+				So(builder.maxIdle, ShouldEqual, maxIdle)
+			})
+
+			Convey("should assign #Host", func() {
+				host := "localhost:1234"
+				builder := Redis().Host(host)
+
+				So(builder.host, ShouldEqual, host)
+			})
 		})
-	})
+
+		Convey("#Delete", t, func() {
+			Convey("should delete values from redis", func() {
+				store := Redis().Namespace("testing").Build()
+				values := map[string]string{"foo": "bar"}
+
+				// When
+				session, _ := store.Create(values)
+				err := store.Delete(session.Id)
+
+				// Then
+				So(err, ShouldBeNil)
+				So(session, ShouldNotBeNil)
+				So(session.Id, ShouldNotEqual, "")
+
+				value, err := store.Get(session.Id)
+				So(err, ShouldBeNil)
+				So(value, ShouldBeNil)
+			})
+		})
+	}
 }
